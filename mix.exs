a3ac14eee8e150079971aefc60d42b26d3368aea
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2017-05-15 02:46:08 +0200

    add package desc

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Calibex.Mixfile do
     [app: :calibex,
      version: "0.1.0",
      elixir: "~> 1.3",
+     package: package,
+     description: description,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -16,5 +18,18 @@ defmodule Calibex.Mixfile do
 
   defp deps do
     [{:timex, "~> 3.1"}]
+  end
+
+  defp package do
+    [ maintainers: ["Arnaud Wetzel"],
+      licenses: ["The MIT License (MIT)"],
+      links: %{ "GitHub"=>"https://github.com/kbrw/calibex" } ]
+  end
+
+  defp description do
+    """
+    Calibex is an ICal Elixir library which focus in bijective coding/decoding in order to allow
+    ICal transformation, ICal email request and responses, and easy non-standard fields inclusion.
+    """
   end
 end
