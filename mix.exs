50d697af60e52079c7cbefd25553304d2d7fbcf7
Mark Felder <feld@feld.me>
2024-03-13 15:22:59 -0400

    Add credo and dialyxir

--- mix.exs
+++ mix.exs
@@ -19,7 +19,12 @@ defmodule Calibex.Mixfile do
   end
 
   defp deps do
-    [{:timex, "~> 3.1"}, {:ex_doc, ">= 0.0.0", only: :dev}]
+    [
+      {:timex, "~> 3.1"},
+      {:credo, "~> 1.7", only: :dev},
+      {:dialyxir, "~> 1.4", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
   end
 
   defp package do
